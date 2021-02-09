ffe948ddcf43c4b4d032e5042814a307e01c0024
Sergey Safronyuk <safronys@gmail.com>
2021-02-09 19:50:26 +0200

    1

---
+++ src/test/java/net/safronys/weblibrary/WebLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.safronys.weblibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
